2fc927b4bdd476c082806cb4ceb5d521753dd271
Thomas Miller <thomas@TomLaptop.home>
2018-03-06 20:19:04 -0500

    Testing functions and flow control

---
+++ part_3/random_guess.py
@@ -0,0 +1,41 @@
+import random, sys
+
+def number_check(value):
+    try:
+        int(value)
+        return 0
+    except ValueError:
+        print("Please enter your guess as a whole number.")
+        return 1
+
+def guess_try(number):
+    if number < secret_num:
+        print("No, that's too low.")
+        return 1
+    elif number > secret_num:
+        print("Too High!")
+        return 1
+    else:
+        return 0
+    
+secret_num = random.randint(1,20)
+print("I'm thinking of a number between 1 and 20..")
+guess=''
+while not guess:
+    print("Guess?")
+    guess= input()
+    if guess == 'exit':
+        print("Exiting...")
+        sys.exit()
+    if guess == '':
+        print("No number entered!")
+    if number_check(guess) == 0:
+        guess=int(guess)
+        success=guess_try(guess)
+        if success == 0:
+            print("You got it!")
+            sys.exit()
+        else:
+            guess=''        
+    else:
+        guess=''
